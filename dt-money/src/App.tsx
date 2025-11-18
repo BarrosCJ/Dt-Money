37612b7ff8b767ed8f539d0954f847432d512027
Cleiton Barros <cleiton.junior@aluno.faculdadeimpacta.com.br>
2025-11-18 19:34:32 -0300

    Primeiro commit

---
+++ dt-money/src/App.tsx
@@ -0,0 +1,19 @@
+import { defaultTheme } from "./styles/themes/default";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./global";
+import { Transactions } from "./pages/Transactions";
+import { TransactionsProvider } from "./contexts/TransactionsContext";
+
+export function App() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
+    </ThemeProvider>
+  )
+}
+
+
